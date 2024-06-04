7930ba8e610173c1f6dd1b8aae4f25c28d1cbacd
EBPO <deminform@gmail.com>
2024-06-04 23:02:11 +0200

    New project: Password Generator

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
-print('Hello World!')
-print('Hello Git!')
+letters_count = int(input('How many letters would you like in your password?\n'))
+symbol_count = int(input('How many symbols would you like?\n'))
+numbers_count = int(input('How many numbers would you like?\n'))
+
+symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '-', '=', '}', '{', '[', ']', '|', '\\', ':', ';', '"', '\'', '<', '>', ',', '.', '?', '/', '~', '`']
+chars_uppercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+chars_lowercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+password = None
+
+def symbol_generator(count):
+    return 
+    
+def char_uppercase_generator(count):
+    return 
+
+def char_lowercase_generator(count):
+    return 
+
+def number_generator(count):
+    return
+
+
+print(f'Here is your password: {password}') 
+
+
